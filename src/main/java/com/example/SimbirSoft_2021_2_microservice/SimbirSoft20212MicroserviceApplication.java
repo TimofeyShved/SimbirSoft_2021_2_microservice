fda0d8c3ccd3dfb2f12cf83f105368f14e247ac2
Швед Тимофей Юрьевич <timofeya123@inbox.ru>
2021-07-30 11:35:26 +0600

    StartProject

---
+++ src/main/java/com/example/SimbirSoft_2021_2_microservice/SimbirSoft20212MicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimbirSoft_2021_2_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbirSoft20212MicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimbirSoft20212MicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SimbirSoft_2021_2_microservice/SimbirSoft20212MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimbirSoft_2021_2_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimbirSoft20212MicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
