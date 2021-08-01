43069316187946a8c799756480535e962ac93f51
Тимофей <68896489+timofeya123@users.noreply.github.com>
2021-08-01 14:25:04 +0600

    UserEntity

---
+++ src/main/java/com/example/SimbirSoft_2021_2_microservice/entity/UserEntity.java
@@ -0,0 +1,97 @@
+package com.example.SimbirSoft_2021_2_microservice.entity;
+
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name="user_entity")
+public class UserEntity { // ----------------------------------------------- наш с вами пользователь
+
+    // ----------------------------------------------- переменные
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // автогенерация значений ключа
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "patronymic")
+    private String patronymic;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    public UserEntity(){ // конструктор
+    }
+
+    public UserEntity(Long userId, String firstName, String lastName, String patronymic, String email, String password) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.patronymic = patronymic;
+        this.email = email;
+        this.password = password;
+    }
+
+    // ----------------------------------------------- гетеры и сетеры
+
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
