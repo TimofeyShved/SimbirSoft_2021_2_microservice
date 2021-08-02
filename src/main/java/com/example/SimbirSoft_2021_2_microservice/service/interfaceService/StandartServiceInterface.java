e8ab1a036c39d822ad015b0c060c8047995bdc03
Тимофей <68896489+timofeya123@users.noreply.github.com>
2021-08-02 14:45:29 +0600

    StandartServiceInterface

--- src/main/java/com/example/SimbirSoft_2021_2_microservice/service/interfaceService/StandartServiceInterface.java
+++ src/main/java/com/example/SimbirSoft_2021_2_microservice/service/interfaceService/StandartServiceInterface.java
@@ -1,4 +1,9 @@
 package com.example.SimbirSoft_2021_2_microservice.service.interfaceService;
 
-public interface StandartServiceInterface {
+public interface StandartServiceInterface<T> {
+    public T registration(T o) throws Exception;
+    public <S> S getAll() throws Exception;
+    public <S> S getOne(Long id) throws Exception;
+    public <S> S deleteOne(Long id) throws Exception;
+    public T updateOne(Long id, T o) throws Exception;
 }
