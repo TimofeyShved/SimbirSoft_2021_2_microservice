72fe2700a2bce6740800e70ff53210dbaf83c080
Тимофей <68896489+timofeya123@users.noreply.github.com>
2021-08-03 14:46:44 +0600

    UserServiceInterface

--- src/main/java/com/example/SimbirSoft_2021_2_microservice/service/interfaceService/UserServiceInterface.java
+++ src/main/java/com/example/SimbirSoft_2021_2_microservice/service/interfaceService/UserServiceInterface.java
@@ -1,4 +1,7 @@
 package com.example.SimbirSoft_2021_2_microservice.service.interfaceService;
 
+import com.example.SimbirSoft_2021_2_microservice.entity.UserEntity;
+
 public interface UserServiceInterface {
+    UserEntity findByEmail(String email) throws UserNotFoundException;
 }
