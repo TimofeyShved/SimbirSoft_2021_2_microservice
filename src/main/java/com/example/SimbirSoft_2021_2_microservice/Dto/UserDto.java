69c096ad0a2074ef1489b1f52dc119d31e6584d4
Тимофей <68896489+timofeya123@users.noreply.github.com>
2021-08-04 14:48:17 +0600

    UserDto

--- src/main/java/com/example/SimbirSoft_2021_2_microservice/Dto/UserDto.java
+++ src/main/java/com/example/SimbirSoft_2021_2_microservice/Dto/UserDto.java
@@ -1,4 +1,91 @@
 package com.example.SimbirSoft_2021_2_microservice.Dto;
 
-public class UserDto {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.persistence.*;
+
+@Schema(description = "Люди")
+public class UserDto { // ----------------------------------------------- наш с вами пользователь
+
+    // ----------------------------------------------- переменные
+    @Schema(description = "Id человека")
+    private Long userId;
+
+    @Schema(description = "Имя")
+    private String firstName;
+
+    @Schema(description = "Фамилия")
+    private String lastName;
+
+    @Schema(description = "Отчество")
+    private String patronymic;
+
+    @Schema(name = "email")
+    private String email;
+
+    @Schema(name = "password")
+    private String password;
+
+    public UserDto(){ // конструктор
+    }
+
+    public UserDto(String firstName, String lastName, String patronymic, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.patronymic = patronymic;
+        this.email = email;
+        this.password = password;
+    }
+
+    // ----------------------------------------------- гетеры и сетеры
+
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+
