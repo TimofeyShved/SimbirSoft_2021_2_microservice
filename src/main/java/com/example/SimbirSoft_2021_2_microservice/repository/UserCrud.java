8568f58202a59fefc639db9af838309134eea906
Тимофей <68896489+timofeya123@users.noreply.github.com>
2021-08-15 15:04:03 +0600

    UserCrud

--- src/main/java/com/example/SimbirSoft_2021_2_microservice/repository/UserCrud.java
+++ src/main/java/com/example/SimbirSoft_2021_2_microservice/repository/UserCrud.java
@@ -1,4 +1,12 @@
 package com.example.SimbirSoft_2021_2_microservice.repository;
 
-public interface UserCrud {
+import com.example.SimbirSoft_2021_2_microservice.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserCrud extends JpaRepository<UserEntity, Long> {
+    UserEntity findByUserId(Long userId);
+    UserEntity findByFirstNameAndLastName(String firstName, String lastName);
+    UserEntity findByEmail(String email);
 }
