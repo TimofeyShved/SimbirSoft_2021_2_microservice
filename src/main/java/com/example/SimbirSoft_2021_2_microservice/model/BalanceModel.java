b23b049c6ab40663acbae4b7baf7ef53d1cb4f1c
Тимофей <68896489+timofeya123@users.noreply.github.com>
2021-08-19 16:08:07 +0600

    BalanceModel

--- src/main/java/com/example/SimbirSoft_2021_2_microservice/model/BalanceModel.java
+++ src/main/java/com/example/SimbirSoft_2021_2_microservice/model/BalanceModel.java
@@ -1,4 +1,41 @@
 package com.example.SimbirSoft_2021_2_microservice.model;
 
+import com.example.SimbirSoft_2021_2_microservice.Dto.BalanceDto;
+import lombok.Data;
+
+@Data
 public class BalanceModel {
+
+    // ----------------------------------------------- переменные
+    private Long balanceId;
+    private Long userId;
+
+    public BalanceModel() {
+    }
+
+    public BalanceModel(Long balanceId, Long userId) {
+        this.balanceId = balanceId;
+        this.userId = userId;
+    }
+
+    public BalanceModel(BalanceDto x) {
+        this.balanceId = x.getBalanceId();
+        this.userId = x.getUserId();
+    }
+
+    public Long getBalanceId() {
+        return balanceId;
+    }
+
+    public void setBalanceId(Long balanceId) {
+        this.balanceId = balanceId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
